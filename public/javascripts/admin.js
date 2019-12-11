8b0f58ffc5575a4bb7ef4b311e8743f8a7a23824
Tanner Pickard <pickardtanner@gmail.com>
2019-12-11 16:46:36 +0000

    Savepoint 5

---
+++ public/javascripts/admin.js
@@ -0,0 +1,84 @@
+/*global Vue*/
+/*global fetch*/
+/*global axios*/
+var app = new Vue({
+  el: '#app',
+  data: {
+    fighters: [],
+    findFighter: "",
+    attack: 0,
+    speed: 0,
+    armor: 0,
+    health: 40,
+    creator: "",
+    name: "",
+    deleteCheck: false,
+    creatorMaster: "",
+    creatorCheck: "",
+    addition: false
+  },
+  created() {
+    this.getFighters()
+  },
+  methods: {
+    async getFighters() {
+      try {
+        let response = await axios.get("/api/fighters");
+        this.fighters = response.data;
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async checkName(creatorCheck) {
+      if (creatorCheck === this.creatorMaster) {
+        this.deleteCheck = true;
+        return;
+      }
+      else {
+        return;
+      }
+    },
+    async addFighter() {
+      try {
+        this.addition = true;
+          let r2 = await axios.post('/api/fighters', {
+            name: this.name,
+            armor: this.armor,
+            health: this.health,
+            attack: this.attack,
+            speed: this.speed,
+            creator: this.creator
+          });
+          this.addFighter = r2.data;
+          this.findFighter = this.addFighter;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    selectFighter(fighter) {
+      this.findFighter = fighter;
+      this.creatorMaster = fighter.creator;
+    },
+    async deleteFighter(findFighter) {
+      try {
+        let response = axios.delete("/api/fighters/" + findFighter._id);
+        this.findFighter = null;
+        this.getFighters();
+        this.deleteCheck = false;
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    compCreator() {
+        if (this.creatorCheck === this.creatorMaster) {
+            this.deleteCheck = true;
+            return;
+        }
+        else {
+            return;
+        }
+    }
+  }
+});
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -13,12 +13,8 @@ var app = new Vue({
     creator: "",
     name: "",
     leaderboard: [],
-    deleteCheck: false,
-    creatorMaster: "",
-    creatorCheck: "",
     hero: "",
     villian: "",
-    addition: false
   },
   created() {
     this.getFighters()
@@ -28,47 +24,6 @@ var app = new Vue({
       try {
         let response = await axios.get("/api/fighters");
         this.fighters = response.data;
-        return true;
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    async checkName(creatorCheck) {
-      if (creatorCheck === this.creatorMaster) {
-        this.deleteCheck = true;
-        return;
-      }
-      else {
-        return;
-      }
-    },
-    async addFighter() {
-      try {
-        this.addition = true;
-          let r2 = await axios.post('/api/fighters', {
-            name: this.name,
-            armor: this.armor,
-            health: this.health,
-            attack: this.attack,
-            speed: this.speed,
-            creator: this.creator
-          });
-          this.addFighter = r2.data;
-          this.findFighter = this.addFighter;
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    selectFighter(fighter) {
-      this.findFighter = fighter;
-      this.creatorMaster = fighter.creator;
-    },
-    async deleteFighter(findFighter) {
-      try {
-        let response = axios.delete("/api/fighters/" + findFighter._id);
-        this.findFighter = null;
-        this.getFighters();
-        this.deleteCheck = false;
         return true;
       } catch (error) {
         console.log(error);
